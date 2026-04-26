0d4baa50ecdde1b9a1ef5b35ef3d72d87a5dec0a
LaoQZ <57404472+LaoQZ@users.noreply.github.com>
2026-04-26 21:59:49 +0800

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,9 +9,13 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_OPENGL=1
 
+
+mkdir -p ./AppDir/bin/
 tar -xvf /tmp/tmp.tar.gz  -C ./AppDir/bin/
 mv ./AppDir/bin/opencode ./AppDir/bin/opencode-cli
 chmod +x ./AppDir/bin/opencode-cli
+cp -v ./opencode-cli.desktop ./AppDir
+cp -v ./opencode-cli.png ./AppDir
 
 # Deploy dependencies
 quick-sharun \
